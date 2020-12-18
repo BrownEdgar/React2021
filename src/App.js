f03c06d9bd98fc3c9a4e57a4f2a6d546ee2c5336
Seyran-hub <babajanyan.seyran@mail.ru>
2020-12-18 16:57:20 +0400

    add my work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 			<h1>Stugum</h1>
-      <p>stugvec</p>
     </div>
   );
 }
--- src/Components/Seyran/App.js
+++ src/Components/Seyran/App.js
@@ -1,1 +1,25 @@
 //Code here
+import React, {Component} from 'react'
+import Data from './data.json'
+
+ export default class App extends Component {
+     state = {
+         users: Data.users
+     }
+     render () {
+         return (
+             <div>
+                 {Data.users.map(elem =>{
+                     return (
+                         <Developers 
+                         key={elem.id}
+                         name={elem.name}
+                         skills={elem.skills}
+                         phone={elem.phone} />
+                     )
+                 })}
+             </div>
+         )
+     }
+ }
+
