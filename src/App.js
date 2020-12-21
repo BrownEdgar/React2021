fcaddfe2cbb090e0fcdbeb93b454c0dfe7528e40
BrownEdgar <nikogosjanedgar@gmail.com>
2020-12-21 17:07:39 +0400

    add changeClass Example

--- src/App.js
+++ src/App.js
@@ -6,20 +6,41 @@ import Developers from './Developers'
 export default class App extends Component {
 	state = {
 		users: ["Edgar", "Seyran", "Lucy", "ArtyomZ", "Artyom"],
-		isShow: false
+		isShow: false,
+		timeToChange:false,
 	}
 	handleClick(){
 		let users = this.state.users;
 		this.setState({ users, isShow: !this.state.isShow  });
 	}
+	deleter(id){
+		let users = this.state.users;
+		users.splice(id, 1);
+		this.setState({ users, isShow: this.state.isShow });
+	}
+	handleChangeClass(){
+		this.setState((prevState) => {
+			return {
+				...prevState,
+				timeToChange: !prevState.timeToChange
+			}
+	})
+}
 	render() {
 		
 		return (
 			<div className="main">
 				{ this.state.isShow && this.state.users.map((elem,index) => {
-					return <Developers name={elem} key={index}/>
+					return <Developers
+					 	name={elem} 
+					 	index= {index}
+						deleter={2326}
+						key={index}
+						timeToChange={this.state.timeToChange}
+						age={25}/>
 				})}
 				<button onClick={this.handleClick.bind(this)}>{this.state.isShow ? "Hide" : "show"}</button>
+				<button onClick={this.handleChangeClass.bind(this)}>Change Class</button>
 			</div>
 		)
 	}
--- src/Developers.jsx
+++ src/Developers.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
 export default function Developers(props) {
-	return <p>{props.name}</p>
+	return (
+		<div class={props.timeToChange ? "ddd" : "red"}>
+			<p>{props.name}</p>
+			<p>{props.age}</p>
+			<button onClick={() => props.deleter(props.index)}>Delete</button>
+		</div>
+	)
 }
+Developers.propTypes = {
+	deleter: PropTypes.func.isRequired,
+	timeToChange: PropTypes.bool.isRequired
+}
+Developers.defaultProps = {
+	age: 150
+};
